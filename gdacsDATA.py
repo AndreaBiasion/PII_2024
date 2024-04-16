a9f68699532064464de019ce770d61fbf02a9854
Omar Chaabani <omarchaabani22@gmail.com>
2024-04-16 10:47:26 +0200

    feat: implement gdacsDATA which fetches the data from GDACS API

---
+++ gdacsDATA.py
@@ -0,0 +1,14 @@
+from gdacs.api import GDACSAPIReader
+import json
+
+client = GDACSAPIReader()
+
+# Fetch the latest earthquake events
+eq_events = client.latest_events(event_type="EQ")
+
+# Convert GeoJSON object to a dictionary
+eq_events_dict = eq_events.dict()
+
+# Write the earthquake events to a JSON file
+with open('events.json', 'w') as f:
+    json.dump(eq_events_dict, f, indent=4)
